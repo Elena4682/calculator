6de1763cecb95a4ae921a94cfbc390cb021c2ae2
Елена <simonaitene@yandex.ru>
2023-04-14 20:35:50 +0300

    Сделано ДЗ

---
+++ src/main/java/pro/sky/java/course2/calculator/Service.java
@@ -0,0 +1,9 @@
+package pro.sky.java.course2.calculator;
+
+public interface Service {
+    String text();
+    String addition(int number1,int number2);
+    String subtraction(int number1,int number2);
+    String multiplication(int number1,int number2);
+    String division(int number1,int number2);
+}
